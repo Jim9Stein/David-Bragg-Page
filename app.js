78ad8fa137a9df6cb29d7f624e9e3fd184743abe
Jim <Jim>
2025-08-11 14:20:32 -0600

    Pushing

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
-async function firstSixIncomplete(userId) {
-        const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
 
-        const result = await promise.json()
-
-        const incompleteTasks = result.filter(element => !element.completed).slice(0, 6)
-
-
-        console.log(incompleteTasks)
-}
-
-
-firstSixIncomplete(6)
   
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-template_pjd47ds
-service_mbpx5ao
-kT_Ps3pkv36G7JdY2
+template_pjd47ds;
+service_mbpx5ao;
+kT_Ps3pkv36G7JdY2;
 
 function contact(event) {
-    event.preventDefault();
-    // emailjs
-    // .sendForm(
-       // 'service_mbpx5ao',
-       // 'template_pjd47ds',
-      // event.target,
-       //'kT_Ps3pkv36G7JdY2'
-   // ).then(() => {
-     //   console.log('this worked1')
-   // })
-   const loading = document.querySelector('.modal__overlay--loading');
-   const success = document.querySelector('.modal__overlay--success');
-   loading.classList += " modal__overlay--visible"
-   setTimeout(() => {
-    loading.classList.remove("modal__overlay--visible")
-    console.log('it worked 1')
-   }, 1000);
-    
+  event.preventDefault();
+  const loading = document.querySelector("modal__overlay--loading");
+  const success = document.querySelector("modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_mbpx5ao",
+      "template_pjd47ds",
+      event.target,
+      "kT_Ps3pkv36G7JdY2"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at James@TeamStein.com"
+      );
+    });
 }
-
