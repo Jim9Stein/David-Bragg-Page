2405a01fd72d085a02fbb25b299df22a28ae23c5
Jim <Jim>
2025-08-11 18:33:05 -0600

    dark push

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-template_pjd47ds;
-service_mbpx5ao;
-kT_Ps3pkv36G7JdY2;
+ let isModalOpen = false
+let contrastToggle = false
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
   event.preventDefault();
-  const loading = document.querySelector("modal__overlay--loading");
-  const success = document.querySelector("modal__overlay--success");
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
   emailjs
     .sendForm(
@@ -24,4 +33,14 @@ function contact(event) {
         "The email service is temporarily unavailable. Please contact me directly at James@TeamStein.com"
       );
     });
+}
+
+function toggleModal() {
+    if(isModalOpen) {
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open";
+
 }
